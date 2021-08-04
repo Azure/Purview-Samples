f1085702bb55675b8de8bf21eedd003ff65ae51c
abandyop <p12arindamb@iimahd.ernet.in>
2021-08-04 06:11:20 +0530

    Update PurviewKickstart.ps1

--- PurviewKickstart.ps1
+++ PurviewKickstart.ps1
@@ -1,8 +1,15 @@
 param (
+    [Parameter(Mandatory=$true)]
     [string]$CatalogName,
+    [Parameter(Mandatory=$true)]
+    [string]$Location,
+    [Parameter(Mandatory=$true)]
     [string]$TenantId,
+    [Parameter(Mandatory=$true)]
     [string]$SubscriptionId,
+    [Parameter(Mandatory=$true)]
     [string]$ResourceGroup,
+    [Parameter(Mandatory=$true)]
     [string]$CatalogResourceGroup,
     [string]$StorageBlobName = $ResourceGroup + "adcblob",
     [string]$AdlsGen2Name = $ResourceGroup + "adcadls",
@@ -14,12 +21,15 @@ param (
                 -SubscriptionId $SubscriptionId `
                 -TenantId $TenantId
 
-    Write-Host "Creating Azure Resource Group for Purview Account...."
+    Write-Host "Creating Azure Resource Group for Purview Account.... [ " $CatalogResourceGroup " ] "
     New-AzResourceGroup `
         -Name $CatalogResourceGroup `
-        -Location "East US"
+        -Location $Location
         
-    Write-Host "Creating Purview Account...."
+    $PurviewTemplate = Get-Content -Path .\purview_template.json
+    $PurviewTemplate -replace 'PURVIEW_ACCOUNT_NAME_CHANGE_BEFORE_RUNNING', $CatalogName | Set-Content -Path .\purview_template.json
+    
+    Write-Host "Creating Purview Account.... [ " $CatalogName " ] "
     New-AzResourceGroupDeployment `
 		-ResourceGroupName $CatalogResourceGroup `
 		-TemplateFile .\purview_template.json
